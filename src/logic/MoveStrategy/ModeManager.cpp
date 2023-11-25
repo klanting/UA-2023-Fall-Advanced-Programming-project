7ef2935dcc3e3535c3fb3a6c9a97235512f8e898
tibov <tibo.verreycken@telenet.be>
2023-11-25 17:44:11 +0100

    also made mode manager

--- src/logic/MoveStrategy/ModeManager.cpp
+++ src/logic/MoveStrategy/ModeManager.cpp
@@ -6,5 +6,16 @@
 
 namespace Logic {
     namespace Move {
+        ModeManager::ModeManager(std::unique_ptr<Mode> mode){
+            strategy = std::unique_ptr<Mode>(std::move(mode));
+        }
+
+        Vector2D ModeManager::getDirection(const Vector2D &to_pacman, const std::vector<Vector2D> &options) const {
+            return strategy->getDirection(to_pacman, options);
+        }
+
+        void ModeManager::setStrategy(std::unique_ptr<Mode> mode) {
+            ModeManager::strategy = std::unique_ptr<Mode>(std::move(mode));
+        }
     } // Logic
 } // Move
--- src/logic/MoveStrategy/ModeManager.h
+++ src/logic/MoveStrategy/ModeManager.h
@@ -4,12 +4,20 @@
 
 #ifndef PROJECTPACMAN_MODEMANAGER_H
 #define PROJECTPACMAN_MODEMANAGER_H
-
+#include "Mode.h"
+#include "memory"
 namespace Logic {
     namespace Move {
 
         class ModeManager {
+        public:
+            ModeManager(std::unique_ptr<Mode> mode);
+            Vector2D getDirection(const Vector2D& to_pacman, const std::vector<Vector2D>& options) const;
 
+            void setStrategy(std::unique_ptr<Mode> mode);
+
+        private:
+            std::unique_ptr<Mode> strategy;
         };
 
     } // Move
